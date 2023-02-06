e747aea8151956aae539f7bae917cc56f4686e97
unknown <ismayilrha@code.edu.az>
2023-02-06 22:12:02 +0400

    added new task

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-console.log()
-
-num = 2;
-
-
-
-meyve = ["alma","armud"];
+let inputUsername = document.getElementById("inputUsername")
+let labelUsername = document.getElementById("usernameLabel")
+let passwordInput = document.getElementById("passwordInput")
 
 
 
+function HoverLabel()
+{
+   labelUsername.style.top = '47px';
+   labelUsername.style.left = '175px'
 
-for (const key in meyve) {
-   console.log(key)
 }
+
+function ResetLabel()
+{
+   labelUsername.style.top = '72px';
+   labelUsername.style.left = '187px'
+}
+
+function ShowPassword()
+{
+   passwordInput.type = "text"
+}
+
+function HidePassword()
+{
+   passwordInput.type = 'password'
+}
+
+
+
+const list = document.getElementById('list');
+
+ const array = []
+
+ fetch('https://jsonplaceholder.typicode.com/comments/')
+ .then(response => response.json())
+ .then(json => {list.innerHTML = json.email})
+  
+
+
+
+
+
+
+
